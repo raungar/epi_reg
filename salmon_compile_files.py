986e634a906bfdc9b590954cc8ce791c116e4bda
Rachel Ungar <rachelungar@gmail.com>
2019-03-04 14:21:47 -0800

    added metadata loop, removed  chip/rna functions
    added metadata loop that properly calls a new file (scripts/run_epireg.sh) which will then call the rest of the relevant information separately (done so can submit as jobs). took out chip/rna computation and moved to new files (rna_quant.py, chip_peaks.py)

--- salmon_compile_files.py
+++ salmon_compile_files.py
@@ -11,148 +11,62 @@ import pandas as pd
 from pyliftover import LiftOver
 import gzip
 
-print("HI??")
-
-def do_salmon(paired,R1,R2):
-	#build index if it does not exist
-	if not os.path.exists("output/hs.grch38.index"):
-		#download grch38 if file does not exist
-		if not os.path.exists("output/Homo_sapiens.GRCh38.cdna.all.fa.gz"): 
-			print("Downloading transcriptome fasta")
-			os.system("wget -P output ftp://ftp.ensembl.org/pub/release-95/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz")
-			print("Download complete")
-		#build salmon index
-		print("building salmon index")
-		os.system("salmon index -t output/Homo_sapiens.GRCh38.cdna.all.fa.gz -i output/hs.grch38.index")
-		print("salmon index built")
-
-	#create quants directory for salmon quantification if it odes not exist
-	if not os.path.exists("output/quants"):
-		os.mkdir("output/quants")
-
-	#perform salmon quantification on single end or paired end data
-	print("salmon quanitification")
-	if(paired=="False"):
-		print("single end salmon")
-		os.system(str("salmon quant -i output/hs.grch38.index -l A -r output/rna/"+R1+".fastq.gz -p 8 -o output/quants/salmon_"+R1))
-	else:
-		print("paired end salmon")
-		os.system(str("salmon quant -i output/hs.grch38.index -l A -1 output/rna/"+R1+".fastq.gz -2 output/rna/"+R2+".fastq.gz -p 8 -o output/quants/salmon_"+R1+"_"+R2))
-
-
-#make bins for chip peak analysis
-def make_bins(chr,start_init, end_init):
-	start=str(start_init-50000)
-	end=str(end_init+50000)
-	
-	os.system(str("echo "+chr)+"\t"+start+"\t"+end+str(" | sed 's/\s/\t/g' > output/chr_file_"+chr+"_"+start+"_"+end+".txt"))
-	os.system(str("bedtools makewindows -b output/chr_file_"+chr+"_"+start+"_"+end+'''.txt -w 500 | awk '{fix=$2+1; print $1"\t"fix"\t"$3e}' > output/bin_file_'''+chr+"_"+start+"_"+end+".bed"))
-	os.remove("output/chr_file_"+chr+"_"+start+"_"+end+".txt")
-
-
-#get peak score
-def get_peaks(chr,start_init, end_init,chip_file_id):
-	#region is 50,000kb +/- of start and stop
-	start=str(start_init-50000)
-	end=str(end_init+50000)
-
-	#if the directory does not exist, create it
-	if not os.path.exists("output/chip_peaks"):
-		os.mkdir("output/chip_peaks")
-	
-	#get peaks at intersection with bin file
-	os.system(str("bedtools intersect -wao -a  output/bin_file_"+chr+"_"+start+"_"+end+".bed -b output/rna/"+chip_file_id+".bed.gz > output/chip_peaks/"+chip_file_id+".bed"))
-	#sort for merge step
-	os.system(str("sort -k1,1 -k2,2n output/chip_peaks/"+chip_file_id+".bed > output/chip_peaks/sorted_"+chip_file_id+".bed"))
-	os.system(str("bedtools merge -i output/chip_peaks/sorted_"+chip_file_id+".bed -c 14 -o max > output/chip_peaks/merged_"+chip_file_id+".bed"))
-	os.remove(str("output/chip_peaks/sorted_"+chip_file_id+".bed"))
-	os.remove(str("output/chip_peaks/"+chip_file_id+".bed"))
-
-
-
-def hg19_to_GRCh38(id):
-	if not os.path.exists("output/hg19ToHg38.over.chain.gz"):
-		urllib.request.urlretrieve("http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz", "output/hg19ToHg38.over.chain.gz")
-		urllib.request.urlretrieve("http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/liftOver","output/liftOver")
-		os.chmod("output/liftOver",777) #give permissions to operate on
-	#os.system(str("output/liftOver output/rna/"+file+".bed.gz output/hg19ToHg38.over.chain.gz output/rna/lifted_"+file+".bed.gz unMapped"))
-	print("converting hg19 to grch38")
-	read_file=str("output/rna/"+id+".bed.gz")
-	write_file=str("output/rna/GRCh38_"+id+".bed.gz")
-	lo=LiftOver("output/hg19ToHg38.over.chain.gz")
-
-
-	with gzip.open(read_file,"rt") as bed_file:
-		with gzip.open(write_file,"wt") as write_bed:
-			for bedline in bed_file:
-				this_bedline=str(bedline).strip().split("\t")
-				#grab converted coordinates
-				coord1=lo.convert_coordinate(str(this_bedline[0]),int(this_bedline[1]))
-				coord2=lo.convert_coordinate(str(this_bedline[0]),int(this_bedline[2]))
-				#if not found, just don't print
-				if not coord1:
-					continue
-				if not coord2:
-					continue
-				#replace coordinates are write to new file
-				output_line=("\t".join([str(coord1[0][0]),str(coord1[0][1]),str(coord2[0][1]),this_bedline[3],
-						this_bedline[4],this_bedline[5],this_bedline[6],this_bedline[7],
-						this_bedline[8],this_bedline[9]]))
-				write_bed.write(output_line+"\n")
-	print("Done: "+write_file)
-
-#gets transcript ids and location for ensembl gene name
-#and returns this information as a dictionary (keys=enst_id,vals=information list)
-def ensg_to_enst(gene_id):
-	#define biomart parameters
-	mart_name = "ensembl"
-	dataset = "hsapiens_gene_ensembl"
-	attributes=["ensembl_transcript_id","chromosome_name","start_position","end_position"]
-	filters={"ensembl_gene_id":[gene_id]}
-	#using biomartpy, obtain pandas df of requested information
-	biomart_df = bm.make_lookup(mart_name=mart_name, dataset=dataset, attributes=attributes, filters=filters)
-	#transform this data frame to a dictionary where the row names (enst_id) are the keys
-	#and the values are the rest of the requested information as a list
-	enst_dic=biomart_df.to_dict("index")
-	return(enst_dic)
-
-def get_rna_quant(enst_id):
-	#get number of max matches so once all ids are found they can stop running through them
-	my_file="output/quants/salmon_ENCFF091UZU_ENCFF163DLM/quant.sf"
-	with open(my_file, "r") as quant_file:
-		for quant_line in quant_file:
-			found=-1
-			for this_id in enst_id:
-				if(this_id) in quant_line:
-					found=this_id
-			#if any(this_id in quant_line for this_id in enst_id):
-			if(found !=-1 ):
-				print(found)
-				enst_id.remove(found) #remove to save time
-				print(quant_line.strip())
-				#if all enst_id are found, stop searching
-				if(len(enst_id)==0):
-					break				
-def combine_files(start,stop):
-	rpkm=100*tpm/(stop-start)
-	
-
 def main():
-	#os.system("conda activate salmon")
+	cell_type_dic={}
 
-	#os.system("source ~/bisque/bqenv/bin/activate")
+	chr="chr6"
+	start=108559823
+	end=108684774
+	gene="ENSG00000118689"
 
 	md_file=sys.argv[1]
 	with open(md_file) as metadata:
 		for md_line in metadata:
 			md_line_split=md_line.split("\t")
-			id=md_line_split[1]
+			id=md_line_split[0]
 			assay=md_line_split[4]
 			cell_type=md_line_split[6]
 			histone_mark=md_line_split[12].split("-")[0]
 			paired_end=md_line_split[29]			
+			pair2=md_line_split[30]
+			assembly=md_line_split[37]
+			
+			if(cell_type in cell_type_dic.keys()):
+				cell_type_dic[cell_type]+=1
+			else:
+				cell_type_dic[cell_type]=1
 
-			print("\t".join([id,assay,cell_type,histone_mark,paired_end]))
+
+			if not(paired_end):
+				R1=id
+				R2=-1
+			else:
+				if(paired_end==1):
+					R1=id
+					R2=pair2
+
+			rename_cell_type=cell_type.replace(" ", "-")
+			rename_cell_type=rename_cell_type.replace("'","")
+			print("THIS IS MY ASSAY: "+assay)
+			if(assay=="ChIP-seq"):
+				#OUTFILE, bed_id, chr, start, end, assemble
+				outfile_name=str(assay+"_"+id+"_"+rename_cell_type+"_"+histone_mark+"_"+str(cell_type_dic[cell_type]))
+				os.system("sh scripts/run_epireg.sh "+outfile_name+" "+id+" "+chr+" "+str(start)+" "+str(end)+" "+assembly)
+				#os.system("sh scripts/run_epireg.sh "+id+" "+assay+" "+cell_type+" "+str(cell_type_dic[cell_type])+" "+histone_mark+" "+assembly)
+				#run chip
+
+			if(assay=="RNA-seq"):
+				outfile_name=str(assay+"_"+id+"_"+rename_cell_type+"_"+str(cell_type_dic[cell_type]))
+				##outfile, ensg_id, R1, R2
+				os.system("sh scripts/run_epireg.sh "+outfile_name+" "+gene+" "+R1+" "+str(R2))
+				#os.system("sh scripts/run_epireg.sh "+id+" "+assay+" "+cell_type+" "+str(cell_type_dic[cell_type])+" "+R1+" "+str(R2))
+				#os.system("sh scripts/run_epireg.sh "+id+" "+assay+" "+cell_type+" "+str(cell_type_dic[cell_type])+" "+R1+" "+str(R2))
+				#get rna
+				#os.system(sh scripts/run_epireg.sh id assay cell_type cell_type_dic[cell_type] R1 R2)
+
+
+	#	print(cell_type_dic)
+			#print("\t".join([id,assay,cell_type,histone_mark,paired_end]))
 
 	#OUTLINE: 
 	#cell type dic
@@ -169,7 +83,7 @@ def main():
 		#is hg19? hg19togrch38 that
 		#is paired?
 		#salmon
-	#matchy matchy
+	#match
 		#below are col names k
 		#bin deets (chr a:1,2) and quants from file
 			#for each line, check if rna quants in there
@@ -181,30 +95,14 @@ def main():
 				
 				#(WELLL RLLY>>>>> put in a line then print out the whole line u know)
 		
-
-
-	#s=Estimated average fragment length
-	#l=Estimated average fragment length
-	#these are kallisto parameters, required for single end.
-	#if paired end kallisto calculates this, pass -1, -1 to symbolize paired end
-	s=200
-	l=10
-	#do_kallisto(s,l)
-	paired="True"
-	#do_salmon(paired, "ENCFF163DLM", -1)
-	#do_salmon(paired, "ENCFF001RMC,-1)
-
 	#for paired end, will need to only take the ones with a 1.
 	#if can't find paired file, output an error message!
 	#do_salmon(paired,"ENCFF091UZU","ENCFF163DLM")
 	#do_salmon(paired,"ENCFF000HBA", "ENCFF000HBI")
-	hg19_to_GRCh38("ENCFF327LZT")
+#	hg19_to_GRCh38("ENCFF327LZT")
 
-	chr="chr6"
-	start=108559823
-	end=108684774
 	#bed_file="ENCFF998UVS" #ENCFF327LZT
-	bed_file="ENCFF327LZT"
+#	bed_file="ENCFF327LZT"
 #	make_bins(chr,int(start), int(end)) #do before loop, should only need ot be done once
 #	get_peaks(chr, int(start),int(end),bed_file)
 
