52e8c4aa4d521bb25d8c0177083e185b38b8fcd5
Rachel Ungar <rachelungar@gmail.com>
2019-03-04 15:06:25 -0800

    correct paired end reading, move make_bins, dir checking
    1. corrected pair end reading bug 
    2. move directory and file checkign to main to avoid potential issues in a parallel setting
    3. moved make_bins to this file because it only needs to be called once

--- salmon_compile_files.py
+++ salmon_compile_files.py
@@ -11,13 +11,62 @@ import pandas as pd
 from pyliftover import LiftOver
 import gzip
 
+
+def make_bins(chr,start_init, end_init):
+	start=str(start_init-50000)
+	end=str(end_init+50000)
+	
+	os.system(str("echo "+chr)+"\t"+start+"\t"+end+str(" | sed 's/\s/\t/g' > output/chr_file_"+chr+"_"+start+"_"+end+".txt"))
+	os.system(str("bedtools makewindows -b output/chr_file_"+chr+"_"+start+"_"+end+'''.txt -w 500 | awk '{fix=$2+1; print $1"\t"fix"\t"$3e}' > output/bin_file_'''+chr+"_"+start+"_"+end+".bed"))
+	os.remove("output/chr_file_"+chr+"_"+start+"_"+end+".txt")
+
+
+def make_folders():
+	if not os.path.exists("output"):
+		os.mkdir("output")
+
+	if not os.path.exists("output/chip_peaks"):
+		os.mkdir("output/chip_peaks")
+
+
+	if not os.path.exists("output/hg19ToHg38.over.chain.gz"):
+		urllib.request.urlretrieve("http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz", "output/hg19ToHg38.over.chain.gz")
+		#urllib.request.urlretrieve("http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/liftOver","output/liftOver")
+		os.chmod("output/liftOver",777) #give permissions to operate on
+
+
+	#MAYBE MOVE THE PATH AND FILE CHECKING TO MAIN GIRL OK 
+	#(COULD MAYBE BE MESSED UP WHEN PARALLEL IMPLEMENTED)
+	#build index if it does not exist
+	if not os.path.exists("output/hs.grch38.index"):
+		#download grch38 if file does not exist
+		if not os.path.exists("output/Homo_sapiens.GRCh38.cdna.all.fa.gz"): 
+			print("Downloading transcriptome fasta")
+			os.system("wget -P output ftp://ftp.ensembl.org/pub/release-95/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz")
+			print("Download complete")
+		#build salmon index
+		print("building salmon index")
+		os.system("salmon index -t output/Homo_sapiens.GRCh38.cdna.all.fa.gz -i output/hs.grch38.index")
+		print("salmon index built")
+
+	#create quants directory for salmon quantification if it odes not exist
+	if not os.path.exists("output/quants"):
+		os.mkdir("output/quants")
+
+
+
+
 def main():
+	print("HELLLO PLZ. PLZ :| ")
 	cell_type_dic={}
 
 	chr="chr6"
 	start=108559823
 	end=108684774
 	gene="ENSG00000118689"
+
+	make_folders()
+	make_bins(chr,int(start),int(end))
 
 	md_file=sys.argv[1]
 	with open(md_file) as metadata:
@@ -44,6 +93,8 @@ def main():
 				if(paired_end==1):
 					R1=id
 					R2=pair2
+				else:
+					continue
 
 			rename_cell_type=cell_type.replace(" ", "-")
 			rename_cell_type=rename_cell_type.replace("'","")
@@ -51,14 +102,14 @@ def main():
 			if(assay=="ChIP-seq"):
 				#OUTFILE, bed_id, chr, start, end, assemble
 				outfile_name=str(assay+"_"+id+"_"+rename_cell_type+"_"+histone_mark+"_"+str(cell_type_dic[cell_type]))
-				os.system("sh scripts/run_epireg.sh "+outfile_name+" "+id+" "+chr+" "+str(start)+" "+str(end)+" "+assembly)
+				os.system("sbatch ./scripts/run_epireg.sh "+outfile_name+" "+id+" "+chr+" "+str(start)+" "+str(end)+" "+assembly)
 				#os.system("sh scripts/run_epireg.sh "+id+" "+assay+" "+cell_type+" "+str(cell_type_dic[cell_type])+" "+histone_mark+" "+assembly)
 				#run chip
 
 			if(assay=="RNA-seq"):
 				outfile_name=str(assay+"_"+id+"_"+rename_cell_type+"_"+str(cell_type_dic[cell_type]))
 				##outfile, ensg_id, R1, R2
-				os.system("sh scripts/run_epireg.sh "+outfile_name+" "+gene+" "+R1+" "+str(R2))
+				os.system("sbatch ./scripts/run_epireg.sh "+outfile_name+" "+gene+" "+R1+" "+str(R2))
 				#os.system("sh scripts/run_epireg.sh "+id+" "+assay+" "+cell_type+" "+str(cell_type_dic[cell_type])+" "+R1+" "+str(R2))
 				#os.system("sh scripts/run_epireg.sh "+id+" "+assay+" "+cell_type+" "+str(cell_type_dic[cell_type])+" "+R1+" "+str(R2))
 				#get rna
@@ -83,7 +134,7 @@ def main():
 		#is hg19? hg19togrch38 that
 		#is paired?
 		#salmon
-	#match
+	#matchy matchy
 		#below are col names k
 		#bin deets (chr a:1,2) and quants from file
 			#for each line, check if rna quants in there
